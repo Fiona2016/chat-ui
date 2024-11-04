6d4dbb02e6bf9beed8466c0cb83ef46af6420245
Fiona <baiyang.feng@outlook.com>
2024-11-04 16:23:35 +0800

    feat: 1. replace icon

--- demo/src/chatProfiles.js
+++ demo/src/chatProfiles.js
@@ -2,11 +2,13 @@ export default [
   {
     id: 'visitor',
     name: '访客',
-    imageUrl: 'https://avatars3.githubusercontent.com/u/1915989?s=230&v=4' // 这里加一下访客链接
+    imageUrl:
+      'https://img.alicdn.com/imgextra/i2/O1CN01aLQksL1z1aOIMW0i1_!!6000000006654-2-tps-200-200.png' // 这里加一下访客链接
   },
   {
     id: 'support', // fixme later 按需修改
     name: 'AI助手',
-    imageUrl: 'https://avatars3.githubusercontent.com/u/37018832?s=200&v=4'
+    imageUrl:
+      'https://img.alicdn.com/imgextra/i2/O1CN01aLQksL1z1aOIMW0i1_!!6000000006654-2-tps-200-200.png'
   }
 ]
